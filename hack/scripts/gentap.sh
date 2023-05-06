ddbf4f77cb3d43eae388c7020b4bbb9f0b73c3a6
ysicing <i@ysicing.me>
2023-05-06 10:18:47 +0800

    feat(action): change action step
    change action step
    
    Signed-off-by: ysicing <i@ysicing.me>

--- hack/scripts/gentap.sh
+++ hack/scripts/gentap.sh
@@ -6,28 +6,27 @@ macosarm64sha=$(cat dist/checksums.txt | grep tiga_darwin_arm64| awk '{print $1}
 linuxamd64sha=$(cat dist/checksums.txt | grep tiga_linux_amd64 | awk '{print $1}')
 linuxarm64sha=$(cat dist/checksums.txt | grep tiga_linux_arm64 | awk '{print $1}')
 
-
 cat > docs/tiga.rb <<EOF
-class TiGA < Formula
+class Tiga < Formula
     desc "Simple and powerful tool for sernior restart engineer"
     homepage "https://github.com/ysicing/tiga"
     version "${version}"
 
     if OS.mac?
       if Hardware::CPU.arm?
-        url "https://github.com/ysicing/tiga/releases/download/#{version}/tiga_darwin_arm64"
+        url "https://ghproxy.com/https://github.com/ysicing/tiga/releases/download/#{version}/tiga_darwin_arm64"
         sha256 "${macosarm64sha}"
       else
-        url "https://github.com/ysicing/tiga/releases/download/#{version}/tiga_darwin_amd64"
+        url "https://ghproxy.com/https://github.com/ysicing/tiga/releases/download/#{version}/tiga_darwin_amd64"
         sha256 "${macosamd64sha}"
       end
     elsif OS.linux?
       if Hardware::CPU.intel?
-        url "https://github.com/ysicing/tiga/releases/download/#{version}/tiga_linux_amd64"
+        url "https://ghproxy.com/https://github.com/ysicing/tiga/releases/download/#{version}/tiga_linux_amd64"
         sha256 "${linuxamd64sha}"
       end
       if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-        url "https://github.com/ysicing/tiga/releases/download/#{version}/tiga_linux_arm64"
+        url "https://ghproxy.com/https://github.com/ysicing/tiga/releases/download/#{version}/tiga_linux_arm64"
         sha256 "${linuxarm64sha}"
       end
     end
